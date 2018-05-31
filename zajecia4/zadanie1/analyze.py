6880e87fa3ff186bde8854c57a33d914982e63b7
ghermann <grzher@st.amu.edu.pl>
2018-05-31 18:18:49 +0200

    modified codes, trees and answers added

--- zajecia4/zadanie1/analyze.py
+++ zajecia4/zadanie1/analyze.py
@@ -30,21 +30,41 @@ clf = tree.DecisionTreeClassifier()
 clf = clf.fit(train_X, train_Y)
 show_plot()
 
-# clf = tree.DecisionTreeClassifier(max_depth=4)
-# clf = clf.fit(train_X, train_Y)
-# show_plot()
+#MAX DEPTH 4
+clf = tree.DecisionTreeClassifier(max_depth=4)
+clf = clf.fit(train_X, train_Y)
+show_plot()
 
+#I
+clf = tree.DecisionTreeClassifier(presort=1, min_samples_leaf=3)
+clf = clf.fit(train_X, train_Y)
+show_plot()
 
- clf = tree.DecisionTreeClassifier(criterion = 'entropy', max_depth=4)
- clf = clf.fit(train_X, train_Y)
-# show_plot()
+#II
+clf = tree.DecisionTreeClassifier(presort=1, min_samples_split=3)
+clf = clf.fit(train_X, train_Y)
+show_plot()
 
- clf = tree.DecisionTreeClassifier(min_samples_split = 5)
- clf = clf.fit(train_X, train_Y)
+#III
+clf = tree.DecisionTreeClassifier(presort=1, min_samples_leaf=10, min_samples_split=10)
+clf = clf.fit(train_X, train_Y)
+show_plot()
 
+#IV
+clf = tree.DecisionTreeClassifier(presort=1, criterion='entropy', splitter='random')
+clf = clf.fit(train_X, train_Y)
+show_plot()
 
- clf = tree.DecisionTreeClassifier(min_samples_leaf  = 5)
- clf = clf.fit(train_X, train_Y)
+#V
+clf = tree.DecisionTreeClassifier(presort=1, max_features='log2')
+clf = clf.fit(train_X, train_Y)
+show_plot()
+
+#VI
+#clf = tree.DecisionTreeClassifier(class_weight=None, criterion='entropy', max_depth=None, max_features=None, max_leaf_nodes=None, min_impurity_decrease=0.0, min_impurity_split=None,             #min_samples_leaf=1, min_samples_split=2, min_weight_fraction_leaf=0.0, presort=False, random_state=None, splitter='best')
+#clf = clf.fit(train_X, train_Y)
+#show_plot()
+
 
 print('TRAIN SET')
 print(confusion_matrix(clf.predict(train_X), train_Y))
